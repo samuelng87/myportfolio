78eb5499c70faa7597f91f2cbcf4f171dd353573
Samuel <59765994+samuelng87@users.noreply.github.com>
2020-08-31 11:33:39 +1000

    human
    image adjust
    phone reponsive

--- humanhistory.js
+++ humanhistory.js
@@ -8,7 +8,7 @@ var time = 5000;
 images [0] = 'cosmo-pic1.jpg';
 images [1] = 'cosmo-pic2.jpg';
 images [2] = 'cosmopic3.jpg';
-images [3] = 'cosmo-pic4.webp';
+images [3] = 'cosmo-pic4.jpg';
 
 
 // change img 
@@ -36,3 +36,21 @@ function printNumber (num) {
 
 printNumber((2020,2021))
 
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
